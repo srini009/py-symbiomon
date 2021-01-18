d36bd63a6b3ddb6cb66ac54de6fc6f24475a5b1d
Srinivasan Ramesh <ramesh.srinivasan09@gmail.com>
2021-01-18 05:05:46 -0500

    Add client python file changes

--- test/client.py
+++ test/client.py
@@ -4,21 +4,26 @@ import sys
 sys.path.append('.')
 sys.path.append('build/lib.linux-x86_64-2.7')
 import os
-from pymargo import MargoInstance
+from pymargo.core import Engine
 from pysymbiomon.client import *
 
-mid = MargoInstance('na+sm')
+mid = Engine('na+sm')
 
 server_addr = sys.argv[1]
 provider_id = int(sys.argv[2])
 
 client = SymbiomonClient(mid)
 addr = mid.lookup(server_addr)
-ph = client.create_metric_handle(addr, 1628746916, provider_id)
 id_ = client.metric_get_id("srini", "testmetric2", ["tag1", "tag2", "tag3"])
-print ("Id is ", id_)
 
-del ph
+#Hardcore id here for testing purposes, and id_ above returns some garbage
+#when translating back up from C++
+id_ = 1628746916
+mh = client.create_metric_handle(addr, id_, provider_id)
+
+samples = mh.metric_fetch(3)
+
+del mh
 del addr
 
 client.finalize()

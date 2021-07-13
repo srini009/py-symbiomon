a87ef9b2d5cc40d57bca5b89e07133831774ab2d
Srinivasan Ramesh <ramesh.srinivasan09@gmail.com>
2021-07-13 04:15:53 -0400

    Remove commented sections

--- pysymbiomon/src/client.cpp
+++ pysymbiomon/src/client.cpp
@@ -56,12 +56,13 @@ static std::vector<std::pair <double, double> > pysymbiomon_remote_metric_fetch(
 
     fprintf(stderr, "Requested for %u samples of metric with name: %s, and ns: %s, returned : %u\n", num_samples_requested, name, ns, num);
 
-    /*if(ret == SYMBIOMON_SUCCESS) {
+    if(ret == SYMBIOMON_SUCCESS) {
        for(int i = 0; i < num; i++) {
           buffer.push_back(std::make_pair(buf[i].val, buf[i].time));
           fprintf(stderr, "Val: %lf, Time: %lf\n", buf[i].val, buf[i].time);
        }
-    }*/
+    }
+
     return buffer;
 }       
 
